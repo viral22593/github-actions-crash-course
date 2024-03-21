7742cee68c212ccb0413212b35e171ed5f970764
Viral patel <viral22593@yahoo.com>
2024-03-21 11:32:31 +0530

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {process.env.REACT_APP_WELCOME_TEXT || "Github Actions Tutorial"} - By
+          {process.env.REACT_APP_WELCOME_TEXT || "Github Actions"} - By
           DevOps Pro
         </p>
         <a
